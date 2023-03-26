39aeaca9a8668eaa0ac2de87e1259f27482d40cf
kincsesb <you@example.com>
2023-03-26 13:54:13 +0200

    Update the Home component with weather API,it needs to be fixed to fetch the data from the backend, add split(T) to import excel component.

--- src/client/Home/Home.jsx
+++ src/client/Home/Home.jsx
@@ -1,10 +1,47 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Home() {
+  const API_KEY = "5155d6bf2239795728e02edd75551e40";
+  const API_ENDPOINT_CURRENT = "https://api.openweathermap.org/data/2.5/weather?q=Hortobagy&appid=" + API_KEY;
+  const API_ENDPOINT_WEEKLY = "https://api.openweathermap.org/data/2.5/forecast?q=Hortobagy&cnt=7&appid=" + API_KEY;
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [weeklyData, setWeeklyData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resultCurrent = await axios(API_ENDPOINT_CURRENT);
+      const resultWeekly = await axios(API_ENDPOINT_WEEKLY);
+      setWeatherData(resultCurrent.data);
+      setWeeklyData(resultWeekly.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="home">
-      <div>This is the Matai Ménes Home Page</div>
+      {weatherData && (
+        <div>
+          <h2>Hortobágyi időjárás</h2>
+          <div className="temp">
+            <p>Ma: {weatherData.weather[0].description}</p>
+            <p>Hőmérséklet: {Math.round(weatherData.main.temp - 273.15)}°C</p>
+          </div>
+        </div>
+      )}
+      {weeklyData && (
+        <div>
+          <h2>Előrejelzés</h2>
+          {weeklyData.list.map((data, index) => (
+            <div key={index} className="temp">
+              <p>{data.dt_txt}</p>
+              <p>{data.weather[0].description}</p>
+              <p>{Math.round(data.main.temp - 273.15)}°C</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
